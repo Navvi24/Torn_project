ead7050aa23023a56f2c6cbdd656766e469a3954
Navvi24 <51583613+Navvi24@users.noreply.github.com>
2020-04-18 12:11:51 +0530

    done
    done

--- index.php
+++ index.php
@@ -105,117 +105,7 @@
 	        		<p class="mb-4 mt-5 pt-5">We have <span class="number" data-number="200000">0</span> great job offers you deserve!</p>
 	            <h1 class="mb-5">Largets Job Site In The World</h1>
 
-							<div class="ftco-search">
-								<div class="row">
-			            <div class="col-md-12 nav-link-wrap">
-				            <div class="nav nav-pills text-center" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-				              <a class="nav-link active mr-md-1" id="v-pills-1-tab" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-1" aria-selected="true">Find a Job</a>
 
-				              <a class="nav-link" id="v-pills-2-tab" data-toggle="pill" href="#v-pills-2" role="tab" aria-controls="v-pills-2" aria-selected="false">Find a Candidate</a>
-
-				            </div>
-				          </div>
-				          <div class="col-md-12 tab-wrap">
-
-				            <div class="tab-content p-4" id="v-pills-tabContent">
-
-				              <div class="tab-pane fade show active" id="v-pills-1" role="tabpanel" aria-labelledby="v-pills-nextgen-tab">
-				              	<form action="#" class="search-job">
-				              		<div class="row no-gutters">
-				              			<div class="col-md mr-md-2">
-				              				<div class="form-group">
-					              				<div class="form-field">
-					              					<div class="icon"><span class="icon-briefcase"></span></div>
-									                <input type="text" class="form-control" placeholder="eg. Garphic. Web Developer">
-									              </div>
-								              </div>
-				              			</div>
-				              			<div class="col-md mr-md-2">
-				              				<div class="form-group">
-				              					<div class="form-field">
-					              					<div class="select-wrap">
-							                      <div class="icon"><span class="ion-ios-arrow-down"></span></div>
-							                      <select name="" id="" class="form-control">
-							                      	<option value="">Category</option>
-							                      	<option value="">Full Time</option>
-							                        <option value="">Part Time</option>
-							                        <option value="">Freelance</option>
-							                        <option value="">Internship</option>
-							                        <option value="">Temporary</option>
-							                      </select>
-							                    </div>
-									              </div>
-								              </div>
-				              			</div>
-				              			<div class="col-md mr-md-2">
-				              				<div class="form-group">
-				              					<div class="form-field">
-					              					<div class="icon"><span class="icon-map-marker"></span></div>
-									                <input type="text" class="form-control" placeholder="Location">
-									              </div>
-								              </div>
-				              			</div>
-				              			<div class="col-md">
-				              				<div class="form-group">
-				              					<div class="form-field">
-								                	<button type="submit" class="form-control btn btn-secondary">Search</button>
-									              </div>
-								              </div>
-				              			</div>
-				              		</div>
-				              	</form>
-				              </div>
-
-				              <div class="tab-pane fade" id="v-pills-2" role="tabpanel" aria-labelledby="v-pills-performance-tab">
-				              	<form action="#" class="search-job">
-				              		<div class="row">
-				              			<div class="col-md">
-				              				<div class="form-group">
-					              				<div class="form-field">
-					              					<div class="icon"><span class="icon-user"></span></div>
-									                <input type="text" class="form-control" placeholder="eg. Adam Scott">
-									              </div>
-								              </div>
-				              			</div>
-				              			<div class="col-md">
-				              				<div class="form-group">
-				              					<div class="form-field">
-					              					<div class="select-wrap">
-							                      <div class="icon"><span class="ion-ios-arrow-down"></span></div>
-							                      <select name="" id="" class="form-control">
-							                      	<option value="">Category</option>
-							                      	<option value="">Full Time</option>
-							                        <option value="">Part Time</option>
-							                        <option value="">Freelance</option>
-							                        <option value="">Internship</option>
-							                        <option value="">Temporary</option>
-							                      </select>
-							                    </div>
-									              </div>
-								              </div>
-				              			</div>
-				              			<div class="col-md">
-				              				<div class="form-group">
-				              					<div class="form-field">
-					              					<div class="icon"><span class="icon-map-marker"></span></div>
-									                <input type="text" class="form-control" placeholder="Location">
-									              </div>
-								              </div>
-				              			</div>
-				              			<div class="col-md">
-				              				<div class="form-group">
-				              					<div class="form-field">
-									                <button type="submit" class="form-control btn btn-secondary">Search</button>
-									              </div>
-								              </div>
-				              			</div>
-				              		</div>
-				              	</form>
-				              </div>
-				            </div>
-				          </div>
-				        </div>
-			        </div>
 	          </div>
 	        </div>
 	    	</div>
@@ -265,50 +155,7 @@
       </div>
     </section>
 
-    <section class="ftco-section">
-    	<div class="container">
-    		<div class="row justify-content-center mb-5 pb-3">
-          <div class="col-md-7 heading-section text-center ftco-animate">
-          	<span class="subheading">Job Categories</span>
-            <h2 class="mb-4">Top Categories</h2>
-          </div>
-        </div>
-        <div class="row">
-        	<div class="col-md-3 ftco-animate">
-        		<ul class="category">
-        			<li><a href="#">Web Development <br><span class="number">354</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        			<li><a href="#">Graphic Designer <br><span class="number">143</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        			<li><a href="#">Multimedia <br><span class="number">100</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        			<li><a href="#">Advertising <br><span class="number">90</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        		</ul>
-        	</div>
-        	<div class="col-md-3 ftco-animate">
-        		<ul class="category">
-        			<li><a href="#">Education &amp; Training <br><span class="number">100</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        			<li><a href="#">English <br><span class="number">200</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        			<li><a href="#">Social Media <br><span class="number">300</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        			<li><a href="#">Writing <br><span class="number">150</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        		</ul>
-        	</div>
-        	<div class="col-md-3 ftco-animate">
-        		<ul class="category">
-        			<li><a href="#">PHP Programming <br><span class="number">400</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        			<li><a href="#">Project Management <br><span class="number">100</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        			<li><a href="#">Finance Management <br><span class="number">222</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        			<li><a href="#">Office &amp; Admin <br><span class="number">123</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        		</ul>
-        	</div>
-        	<div class="col-md-3 ftco-animate">
-        		<ul class="category">
-        			<li><a href="#">Web Designer <br><span class="number">324</span> <span>Open position</span></span><i class="ion-ios-arrow-forward"></i></a></li>
-        			<li><a href="#">Customer Service <br><span class="number">564</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        			<li><a href="#">Marketing &amp; Sales <br><span class="number">234</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        			<li><a href="#">Software Development <br><span class="number">425</span> <span>Open position</span><i class="ion-ios-arrow-forward"></i></a></li>
-        		</ul>
-        	</div>
-        </div>
-    	</div>
-    </section>
+
 
     <section class="ftco-section img" style="background-image: url(images/bg_1.jpg); background-position: top center;">
     	<div class="container">
@@ -328,7 +175,7 @@
 		<section class="ftco-section bg-light">
 			<div class="container">
 				<div class="row">
-					<div class="col-lg-9 pr-lg-5">
+					<div class="col-lg-12 pr-lg-5">
 						<div class="row justify-content-center pb-3">
 		          <div class="col-md-12 heading-section ftco-animate">
 		          	<span class="subheading">Recently Added Jobs</span>
@@ -569,41 +416,6 @@
 		              </div>
 		            </div>
 		          </div><!-- end -->
-		        </div>
-		      </div>
-		      <div class="col-lg-3 sidebar">
-		        <div class="row justify-content-center pb-3">
-		          <div class="col-md-12 heading-section ftco-animate">
-		          	<span class="subheading">Recruitment agencies</span>
-		            <h2 class="mb-4">Top Recruitments</h2>
-		          </div>
-		        </div>
-		        <div class="sidebar-box ftco-animate">
-		        	<div class="border">
-			        	<a href="#" class="company-wrap"><img src="images/company-1.jpg" class="img-fluid" alt="Colorlib Free Template"></a>
-			        	<div class="text p-3">
-			        		<h3><a href="#">Google Company</a></h3>
-			        		<p><span class="number">500</span> <span>Open position</span></p>
-			        	</div>
-		        	</div>
-		        </div>
-		        <div class="sidebar-box ftco-animate">
-		        	<div class="border">
-			        	<a href="#" class="company-wrap"><img src="images/company-2.jpg" class="img-fluid" alt="Colorlib Free Template"></a>
-			        	<div class="text p-3">
-			        		<h3><a href="#">Facebook Company</a></h3>
-			        		<p><span class="number">700</span> <span>Open position</span></p>
-			        	</div>
-			        </div>
-		        </div>
-		        <div class="sidebar-box ftco-animate">
-		        	<div class="border">
-			        	<a href="#" class="company-wrap"><img src="images/company-3.jpg" class="img-fluid" alt="Colorlib Free Template"></a>
-			        	<div class="text p-3">
-			        		<h3><a href="#">IT Programming INC</a></h3>
-			        		<p><span class="number">700</span> <span>Open position</span></p>
-			        	</div>
-			        </div>
 		        </div>
 		      </div>
 				</div>
