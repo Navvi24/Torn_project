aa488542940c176f1d4828ec6d1d3e6b5753ded9
Navvi24 <51583613+Navvi24@users.noreply.github.com>
2020-04-24 20:28:09 +0530

    Our Client section Done
    Done

--- our-clients.php
+++ our-clients.php
@@ -113,7 +113,7 @@ s0.parentNode.insertBefore(s1,s0);
 	    </div>
 	  </nav>
     <!-- END nav -->
-    <div class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');" data-stellar-background-ratio="0.5">
+    <div class="hero-wrap hero-wrap-2" style="background-image: url('images/client_bg.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text align-items-end justify-content-start">
@@ -130,95 +130,38 @@ s0.parentNode.insertBefore(s1,s0);
       <div class="row">
         <div class="col-md-6">
           <div class="team d-md-flex">
-            <div class="img" style="background-image: url(images/person_1.jpg);"></div>
+            <div class="img" style="background-image: url(images/client1.png);"></div>
             <div class="text pl-md-4">
-              <span class="location mb-0">Western City, UK</span>
-              <h2>Danica Lewis</h2>
-              <span class="position">Graduate</span>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <span class="seen">Last Activity 4 months ago</span>
-              <p><a href="#" class="btn btn-primary">Shortlist</a></p>
+              <h2>Couponmonster</h2>
+              <p>Couponmonster.co is an all-inclusive coupon distribution company that ties up with various businesses around the city and brings you the best offers. #usekiyakya?</p>
             </div>
           </div>
         </div>
         <div class="col-md-6">
           <div class="team d-md-flex">
-            <div class="img" style="background-image: url(images/person_2.jpg);"></div>
+            <div class="img" style="background-image: url(images/client2.jpg);"></div>
             <div class="text pl-md-4">
-              <span class="location mb-0">Western City, UK</span>
-              <h2>Danica Lewis</h2>
-              <span class="position">Graduate</span>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <span class="seen">Last Activity 4 months ago</span>
-              <p><a href="#" class="btn btn-primary">Shortlist</a></p>
+              <h2>ProUp</h2>
+              <p>Pro Up is an outcome of passion for the founders who left their corporate careers and started this company. Pro Up offers a whole range of services in the domain of Digital Marketing. The company, all though young, has witnessed rapid growth since inception. Its work has been appreciated by both its clients and the audience. At Pro Up, we donÂ’t just look at the Marketing aspect of a business, but also at solving business problems via Digital Marketing.</p>
             </div>
           </div>
         </div>
         <div class="col-md-6">
           <div class="team d-md-flex">
-            <div class="img" style="background-image: url(images/person_3.jpg);"></div>
+            <div class="img" style="background-image: url(images/client3.jpg);"></div>
             <div class="text pl-md-4">
-              <span class="location mb-0">Western City, UK</span>
-              <h2>Danica Lewis</h2>
-              <span class="position">Graduate</span>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <span class="seen">Last Activity 4 months ago</span>
-              <p><a href="#" class="btn btn-primary">Shortlist</a></p>
+              <h2>Brandgrapher</h2>
+              <p>Brandgrapher provides futuristic product development, marketing, and business automation consultancy and training. It’s an initiative taken by Young Entrepreneurs and is meant for the aspiring ones. Brandgrapher aims at impacting a million teenage, student and side-hustle entrepreneurs, micro-influencers, and bloggers by providing the best tools and strategies to set up a Personal Brand.</p>
             </div>
           </div>
         </div>
         <div class="col-md-6">
           <div class="team d-md-flex">
-            <div class="img" style="background-image: url(images/person_4.jpg);"></div>
+            <div class="img" style="background-image: url(images/client4.png);"></div>
             <div class="text pl-md-4">
-              <span class="location mb-0">Western City, UK</span>
-              <h2>Danica Lewis</h2>
-              <span class="position">Graduate</span>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <span class="seen">Last Activity 4 months ago</span>
-              <p><a href="#" class="btn btn-primary">Shortlist</a></p>
+              <h2>Orbiting</h2>
+              <p>Orbiting.in the new big thing in the field of Social Networking and aims to change the user experience by changing how we use social media. It allows you to interact with people with similar interests by engaging in the same orbits as them. These orbits are both content and location sensitive, so it makes sure that you use social media as per your preferences.</p>
             </div>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="team d-md-flex">
-            <div class="img" style="background-image: url(images/person_5.jpg);"></div>
-            <div class="text pl-md-4">
-              <span class="location mb-0">Western City, UK</span>
-              <h2>Danica Lewis</h2>
-              <span class="position">Graduate</span>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <span class="seen">Last Activity 4 months ago</span>
-              <p><a href="#" class="btn btn-primary">Shortlist</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="team d-md-flex">
-            <div class="img" style="background-image: url(images/person_6.jpg);"></div>
-            <div class="text pl-md-4">
-              <span class="location mb-0">Western City, UK</span>
-              <h2>Danica Lewis</h2>
-              <span class="position">Graduate</span>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <span class="seen">Last Activity 4 months ago</span>
-              <p><a href="#" class="btn btn-primary">Shortlist</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row mt-5">
-        <div class="col text-center">
-          <div class="block-27">
-            <ul>
-              <li><a href="#">&lt;</a></li>
-              <li class="active"><span>1</span></li>
-              <li><a href="#">2</a></li>
-              <li><a href="#">3</a></li>
-              <li><a href="#">4</a></li>
-              <li><a href="#">5</a></li>
-              <li><a href="#">&gt;</a></li>
-            </ul>
           </div>
         </div>
       </div>
