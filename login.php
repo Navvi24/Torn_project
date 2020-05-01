a3aca37ba750cdee91776832d6b7a474090326db
Navvi24 <51583613+Navvi24@users.noreply.github.com>
2020-05-01 17:15:50 +0530

    Done
    Done

--- login.php
+++ login.php
@@ -160,7 +160,7 @@ s0.parentNode.insertBefore(s1,s0);
               </div>
               <div class="row form-group">
                 <div class="col-md-12">
-                  <a href="#">Forget Password</a>
+                  <a href="#"  data-toggle="modal" data-target="#exampleModal">Forget Password</a>
                 </div>
               </div><div class="row form-group">
                 <div class="col-md-12">
@@ -169,6 +169,29 @@ s0.parentNode.insertBefore(s1,s0);
               </div>
 
             </form>
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Forget Password</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <form class="p-5 bg-white">
+                          <label class="font-weight-bold" for="username">Enter your Email</label>
+                          <input type="email" id="fullname" class="form-control" placeholder="Enter your email" name="email" required>
+                          <br>
+                          <center>  <button type="button" class="btn btn-primary">Send Link</button> </center>
+                    </form>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
