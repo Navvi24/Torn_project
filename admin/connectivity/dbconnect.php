df110e3c79f8a1d4579de8a23a3f1abf0f3a0285
piyush2599 <piyush2599@users.noreply.github.com>
2020-05-05 16:24:31 +0530

    fsf

--- admin/connectivity/dbconnect.php
+++ admin/connectivity/dbconnect.php
@@ -6,9 +6,9 @@ $database="adhoc_management"; //Enter database name here
 $servername="localhost"; //Server address
 */
 
-$username="internst_root"; //DB User Name
-$password="de4$#sdf0JU"; //DB User password
-$database="internst_new_web_data"; //Enter database name here
+$username="root"; //DB User Name
+$password=""; //DB User password
+$database="internstorm"; //Enter database name here
 $servername="localhost"; //Server address
 
 // Create connection
